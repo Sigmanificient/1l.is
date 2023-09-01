92b932eba106b727434b374d4dbd2ed689ae7cc4
Clément <clement2104.boillot@gmail.com>
2023-09-01 19:09:47 +0200

    Fix base 36 encode/decode

--- src/onelink/base36.py
+++ src/onelink/base36.py
@@ -1,25 +1,41 @@
 from typing import Final, Optional
+from itertools import combinations
+from string import ascii_lowercase, digits
 
 
-CHAR_LIST: Final[str] = "0123456789abcdefghijklmnopqrstuvwxyz"
-NBR_CHAR: Final[int] = len(CHAR_LIST)
+BASE: Final[str] = digits + ascii_lowercase
+LEN_BASE: Final[int] = len(BASE)
 
 
 def base36_encode(input: int) -> Optional[str]:
     if input < 0:
         return None
-    result = ""
+    result = []
     while input != 0:
-        result += CHAR_LIST[input % NBR_CHAR]
-        input //= NBR_CHAR
-    return result
+        result += BASE[input % LEN_BASE]
+        input //= LEN_BASE
+    result.pop()
+    return "".join(reversed(result))
 
 
 def base36_decode(input: str) -> Optional[int]:
+    if not input.isalnum():
+        return None
+    input = "a" + input.lower()
     result = 0
     for i, c in enumerate(reversed(input)):
-        index = CHAR_LIST.find(c)
-        if index == -1:
-            return None
-        result += index * (NBR_CHAR ** i)
+        result += BASE.index(c) * (LEN_BASE ** i)
     return result
+
+
+def test_encode_decode_4_chars():
+    for letters in combinations(BASE, r=4):
+        word = ''.join(letters)
+        encoded = word
+
+        for _ in range(3):
+            decoded = base36_decode(encoded)
+            assert decoded is not None
+            encoded = base36_encode(decoded)
+            assert encoded is not None
+        assert encoded == word
