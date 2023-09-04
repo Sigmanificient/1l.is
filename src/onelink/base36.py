1e637e6d633722e20ff0425b7ec7fcfb3e1df57b
Clément <clement2104.boillot@gmail.com>
2023-09-04 22:46:56 +0200

    Fix borken base36

--- src/onelink/base36.py
+++ src/onelink/base36.py
@@ -1,41 +1,39 @@
 from typing import Final, Optional
 from itertools import combinations
 from string import ascii_lowercase, digits
+from io import StringIO
 
 
 BASE: Final[str] = digits + ascii_lowercase
 LEN_BASE: Final[int] = len(BASE)
 
 
-def base36_encode(input: int) -> Optional[str]:
-    if input < 0:
-        return None
-    result = []
-    while input != 0:
-        result += BASE[input % LEN_BASE]
-        input //= LEN_BASE
-    result.pop()
-    return "".join(reversed(result))
+def base36_encode(n: int) -> str:
+    r = StringIO()
+    while n:
+        n, t = divmod(n, LEN_BASE)
+        r.write(BASE[t])
+    return r.getvalue()[::-1]
 
 
-def base36_decode(input: str) -> Optional[int]:
-    if not input.isalnum():
-        return None
-    input = "a" + input.lower()
-    result = 0
-    for i, c in enumerate(reversed(input)):
-        result += BASE.index(c) * (LEN_BASE ** i)
-    return result
+def base36_decode(input: str) -> int:
+    return int(input.lower(), 36)
 
 
 def test_encode_decode_4_chars():
     for letters in combinations(BASE, r=4):
         word = ''.join(letters)
-        encoded = word
+        if word[0] == '0':
+            continue
 
+        encoded = word
         for _ in range(3):
-            decoded = base36_decode(encoded)
-            assert decoded is not None
-            encoded = base36_encode(decoded)
-            assert encoded is not None
+            encoded = base36_encode(base36_decode(encoded))
         assert encoded == word
+
+def test_decode_encode():
+    for i in range(1, LEN_BASE ** 3):
+        n = i
+        for _ in range(3):
+            n = base36_decode(base36_encode(n))
+        assert i == n
