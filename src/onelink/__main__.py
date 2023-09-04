ceaeaf0efe9bda93d4fa8d22ccdb6c4f6a02bf31
Sigmanificient <edhyjox@gmail.com>
2023-09-04 23:02:16 +0200

    Make it """work"""
    Co-authored-by: drawbu <clement2104.boillot@gmail.com>

--- src/onelink/__main__.py
+++ src/onelink/__main__.py
@@ -1,31 +1,52 @@
 from quart import Quart
 
+from .base36 import base36_decode, base36_encode
 from .database import get_db
+
+QUERY_REDIRECT = "SELECT redirect FROM link WHERE id = ?"
+QUERY_CHECK_ID = "SELECT name FROM link WHERE redirect = ?"
+QUERY_INSERT_LINK = "INSERT INTO link(name, redirect) VALUES(?, ?)"
+QUERY_LAST_ID = "SELECT IFNULL(id, 1) FROM link ORDER BY id desc LIMIT 1"
+
 
 app = Quart(__name__)
 app.config.update(DATABASE=app.root_path / ".." / ".." / ".db")
 
 
-@app.route("/")
-async def home():
-    script = (app.root_path / ".." / ".." / "seeder.sql").read_text()
-
+@app.route("/create/<path:url>")
+async def home(url: str):
     async with get_db(app) as db:
-        await db.executescript(script)
+        res = tuple(await db.execute_fetchall(QUERY_CHECK_ID, (url,)))
+        print(f"{res=!r}")
 
-        await db.execute(
-            "INSERT INTO link(name, redirect) VALUES(?, ?)",
-            ("test", "test")
-        )
+        if res:
+            return res[0][0]
 
+        last_id = (
+            tuple(await db.execute_fetchall(QUERY_LAST_ID, ()))
+            or ((0,),)
+        )[0][0]
+
+        print(f"{last_id=!r}")
+
+        name = base36_encode(last_id + 1)
+        print(f"{name=!r}")
+
+        await db.execute(QUERY_INSERT_LINK, (name, url))
         await db.commit()
 
-        r = await db.execute_fetchall("SELECT * FROM link")
-        print(r)
-    return "hello"
+    return name
 
 
+@app.route("/<path:path>")
+async def resolve_url(path: str):
+    url_id = base36_decode(path.lower())
+    print(url_id)
 
+    async with get_db(app) as db:
+        redirect = tuple(await db.execute_fetchall(QUERY_REDIRECT, (url_id,)))
+
+    return str(redirect)
 
 def main():
     app.run()
