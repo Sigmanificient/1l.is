9ee9e96452a5f34f4cc2d5fe45b28d65894f62e9
Sigmanificient <edhyjox@gmail.com>
2023-09-05 23:16:05 +0200

    Wrap the db context mananger within a singleton

--- src/onelink/database.py
+++ src/onelink/database.py
@@ -1,11 +1,29 @@
 from pathlib import Path
 import aiosqlite
 from contextlib import asynccontextmanager
+from functools import wraps
 from quart import Quart
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Optional
+
+
+def _singleton(gfunc):
+    db: Optional[aiosqlite.Connection] = None
+
+    @wraps(gfunc)
+    async def wrapped(*args):
+        nonlocal db
+
+        if db is None or not db.is_alive():
+            db = await gfunc(*args).__anext__()
+
+        yield db
+
+    return wrapped
+
 
 
 @asynccontextmanager
+@_singleton
 async def get_db(app: Quart) -> AsyncGenerator[aiosqlite.Connection, None]:
     db_path: Path = app.config["DATABASE"]
 
