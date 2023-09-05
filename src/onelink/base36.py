b86afc39f37c2b1571f1605d145864b144df37e6
Sigmanificient <edhyjox@gmail.com>
2023-09-05 23:49:46 +0200

    Simplify base36 code

--- src/onelink/base36.py
+++ src/onelink/base36.py
@@ -1,27 +1,28 @@
-from typing import Final, Optional
+from functools import partial
+from io import StringIO
 from itertools import combinations
 from string import ascii_lowercase, digits
-from io import StringIO
 
+from typing import Final, Set
 
 BASE: Final[str] = digits + ascii_lowercase
-LEN_BASE: Final[int] = len(BASE)
+BASE_LEN: Final[int] = len(BASE)
+BASE_SET: Final[Set[str]] = set(BASE)
 
 
 def base36_encode(n: int) -> str:
     r = StringIO()
     while n:
-        n, t = divmod(n, LEN_BASE)
+        n, t = divmod(n, BASE_LEN)
         r.write(BASE[t])
     return r.getvalue()[::-1]
 
 
-def base36_decode(input: str) -> int:
-    return int(input.lower(), 36)
+base36_decode = partial(int, base=BASE_LEN)
 
 
-def base36_valid(input: str) -> bool:
-    return all(char in BASE for char in input)
+def base36_valid(s: str) -> bool:
+    return all(map(BASE_SET.__contains__, s))
 
 
 def test_encode_decode_4_chars():
@@ -36,7 +37,7 @@ def test_encode_decode_4_chars():
         assert encoded == word
 
 def test_decode_encode():
-    for i in range(1, LEN_BASE ** 3):
+    for i in range(1, BASE_LEN ** 3):
         n = i
         for _ in range(3):
             n = base36_decode(base36_encode(n))
